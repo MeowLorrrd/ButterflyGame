ee0098be72eb6c8bd547dcb496b43276e19ea9a2
MeowLorrrd <jayola300@gmail.com>
2024-05-13 14:04:25 +0200

    fixed not properly copying files

--- prelink.bat
+++ prelink.bat
@@ -1,2 +1,14 @@
-xcopy /s/y/k .\Assets x64\Debug\Assets
-xcopy /s/y/k .\Assets x64\Release\Assets
+echo d | xcopy /s/y/k .\Assets x64\Debug\Assets
+echo d | xcopy /s/y/k .\Assets x64\Release\Assets
+
+echo f | xcopy /s/y/k .\include\SFML-2.6.1\bin\sfml-audio-d-2.dll x64\Debug
+echo f | xcopy /s/y/k .\include\SFML-2.6.1\bin\sfml-graphics-d-2.dll x64\Debug
+echo f | xcopy /s/y/k .\include\SFML-2.6.1\bin\sfml-network-d-2.dll x64\Debug
+echo f | xcopy /s/y/k .\include\SFML-2.6.1\bin\sfml-system-d-2.dll x64\Debug
+echo f | xcopy /s/y/k .\include\SFML-2.6.1\bin\sfml-window-d-2.dll x64\Debug
+
+echo f | xcopy /s/y/k .\include\SFML-2.6.1\bin\sfml-audio-2.dll x64\Release
+echo f | xcopy /s/y/k .\include\SFML-2.6.1\bin\sfml-graphics-2.dll x64\Release
+echo f | xcopy /s/y/k .\include\SFML-2.6.1\bin\sfml-network-2.dll x64\Release
+echo f | xcopy /s/y/k .\include\SFML-2.6.1\bin\sfml-system-2.dll x64\Release
+echo f | xcopy /s/y/k .\include\SFML-2.6.1\bin\sfml-window-2.dll x64\Release
